efcec3e2fa528b6276c7086156c6e273906adec9
Brenda Franco <brenda.leigh.franco@gmail.com>
2018-03-27 19:54:34 -0700

    Add logic to determine if inputed value is a leap year.

---
+++ js/scripts.js
@@ -0,0 +1,28 @@
+// User-Interface Logic
+$(document).ready(function() {
+  $("form#leap-year").submit(function(event) {
+    event.preventDefault();
+    var inputYear = parseInt($("#year").val());
+    var result = leapYear(inputYear);
+
+    $("#result").text(result);
+  });
+});
+
+// Business Logic
+var leapYear = function(year) {
+  if ((year % 4 ===0) && (year % 100 !==0) || (year % 4 ===0)) {
+    $("#result").text("This is a leap year!");
+  } else {
+    $("#result").text("This is NOT a leap year!");
+  }
+  // if (year % 400 ===0) {
+  //   return true;
+  // } else if (year % 100 ===0) {
+  //   return false;
+  // } else if (year % 4 === 0) {
+  //   return true;
+  // } else {
+  //   return false;
+  // }
+};
